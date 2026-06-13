835192c3b45cafb3744dd1e7dcd7c59ed9113faf
Talo <talo.italo.ng@gmail.com>
2026-06-13 08:01:32 -0500

    Correccion de auditoria

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import { inicializarMenu } from './modulos/menu.js';
 import { inicializarComoFunciona } from './modulos/comoFunciona.js';
 import { inicializarVistaApp } from './modulos/vistaApp.js';
+import { inicializarContacto } from './modulos/contacto.js';
 
 inicializarMenu();
 inicializarComoFunciona();
 inicializarVistaApp();
+inicializarContacto();
